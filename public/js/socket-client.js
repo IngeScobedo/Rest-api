6654ab7305807ad9330d2b07c2c4743b5fe2b4d9
Alan Escobedo <ingescobedoalan4@gmail.com>
2022-10-26 21:53:39 -0500

    Detectar los cambios del estado de los sockets

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,14 +1,21 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
 
+// DOM References
+const lblOnline = document.querySelector('#lblOnline')
+const lblOffline = document.querySelector('#lblOffline')
+
 // io() viene de el script "public/socket.io/socket.io.js"
 const socket = io()
 
 // listeners
 socket.on('connect', () => {
-  console.log('Conectado')
+  console.log('conectado')
+  lblOffline.classList.toggle('hidden')
+  lblOnline.classList.toggle('hidden')
 })
 
 socket.on('disconnect', () => {
-  console.log('Desconectado')
+  lblOffline.classList.toggle('hidden')
+  lblOnline.classList.toggle('hidden')
 })
